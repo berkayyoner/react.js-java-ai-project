fd977be7e3d6b67d29075c6e808301339fa53bcf
Berkay Öner <berkayonerr2002@gmail.com>
2025-09-07 22:35:13 +0300

    Home page is finished and redesigned.

--- front-end/src/pages/Home/Home.js
+++ front-end/src/pages/Home/Home.js
@@ -3,64 +3,123 @@ import { translate } from "../../utils/translate";
 import image from "../../assets/images/profile.png";
 import "./Home.css";
 import useLanguage from "../../hooks/useLanguage";
+import config from "../../config.json";
 
 function Home() {
-  const { lang, changeLang } = useLanguage();  
+  const { lang } = useLanguage();  
+
+  const mailHref = `mailto:${config.email}`;
 
   return (
     <div className="home">
-        <NavBar selectedButton="home" />
-        <div className="cv">
-          <div className="top-section">
+      <NavBar selectedButton="home" />
+
+      <div className="cv">
+        <div className="top-section">
+          <img src={image} alt="Profile" className="profile-pic" />
+
+          <div className="hero-copy">
             <h1 className="title">{translate(lang, "home.title")}</h1>
             <p className="description">{translate(lang, "home.description")}</p>
           </div>
-          <div className="horizontal-line" />
-            <div className="main-columns">
+        </div>
 
-              <div className="column">
-                <h2 className="title">{translate(lang, "home.education")}</h2>
-                <ul className="list">
-                  <li>
-                    <h3 className="title">{translate(lang, "home.educationItem2")}</h3>
-                    <p>{translate(lang, "home.educationItem2Description")}</p>
-                    <p>2022 - 2024</p>
-                  </li>
-                  <li>
-                    <h3 className="title">{translate(lang, "home.educationItem1")}</h3>
-                    <p>{translate(lang, "home.educationItem1Description")}</p>
-                    <p>2020 - 2022</p>
-                  </li>
-                </ul>
+        <div className="horizontal-line" />
+
+        <div className="main-columns">
+          <section className="education-column">
+            <h2 className="title">{translate(lang, "home.education")}</h2>
+            <br/>
+            <ul className="list">
+              <li>
+                <h3 className="title">{translate(lang, "home.educationItem2")}</h3>
+                <p>{translate(lang, "home.educationItem2Description")}</p>
+                <p>{translate(lang, "home.educationItem2Year")}</p>
+              </li>
+              <li>
+                <h3 className="title">{translate(lang, "home.educationItem1")}</h3>
+                <p>{translate(lang, "home.educationItem1Description")}</p>
+                <p>{translate(lang, "home.educationItem1Year")}</p>
+              </li>
+            </ul>
+          </section>
+
+          <div className="vertical-line" aria-hidden="true" />
+
+          <section className="contact-column">
+            <h2 className="title">{translate(lang, "home.contact")}</h2>
+            <div className="contact-section">
+              <br/>
+
+              <div className="contact-item">
+                <h3 className="title">{translate(lang, "home.email")}</h3>
+                <a href={mailHref} className="link">
+                  {config.email}
+                </a>
               </div>
 
-              <a className="vertical-line"/>
+              <br/>
 
-              <div className="column">
-                <h2 className="title">{translate(lang, "home.contact")}</h2>
-                <div className="contact-section">
-                  <div className="contact-item">
-                    <h3 className="title">{translate(lang, "home.email")}</h3>
-                    <a>berkayonerr2002@gmail.com</a>
-                  </div>
-                  <div className="contact-item">
-                    <h3 className="title">{translate(lang, "home.linkedIn")}</h3>
-                    <a href="https://www.linkedin.com/in/berkay-%C3%B6ner-a4599223a/" target="_blank" rel="noopener noreferrer" className="link">{translate(lang, "home.linkedInLink")}</a>
-                  </div>
-                </div>
-              </div>
-
-              <a className="vertical-line"/>
-
-              <div className="column">
-                <h2 className="title">{translate(lang, "home.experiences")}</h2>
-                <ul>
-                  
-                </ul>
+              <div className="contact-item">
+                <h3 className="title">{translate(lang, "home.linkedIn")}</h3>
+                <a
+                  href={config.linkedIn}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link"
+                >
+                  {translate(lang, "home.linkedInLink")}
+                </a>
               </div>
             </div>
-            <img src={image} alt="Profile" className="profile-pic" />
-          </div>
+          </section>
+
+          <div className="vertical-line" aria-hidden="true" />
+
+          <section className="experiences-column">
+            <h2 className="title">{translate(lang, "home.experiences")}</h2>
+            <br/>
+            <ul className="list">
+              <li className="sized-list-item">
+                <h3 className="title">{translate(lang, "home.experienceItem4")}</h3>
+                <p>{translate(lang, "home.experienceItem4Year")}</p>
+                <p>{translate(lang, "home.experienceItem4Location")}</p>
+                <p className="experience-description">
+                  {translate(lang, "home.experienceItem4Description")}
+                </p>
+              </li>
+
+              <li className="sized-list-item">
+                <h3 className="title">{translate(lang, "home.experienceItem3")}</h3>
+                <p>{translate(lang, "home.experienceItem3Year")}</p>
+                <p>{translate(lang, "home.experienceItem3Location")}</p>
+                <p className="experience-description">
+                  {translate(lang, "home.experienceItem3Description")}
+                </p>
+              </li>
+
+              <li className="sized-list-item">
+                <h3 className="title">{translate(lang, "home.experienceItem2")}</h3>
+                <p>{translate(lang, "home.experienceItem2Year")}</p>
+                <p>{translate(lang, "home.experienceItem2Location")}</p>
+                <p className="experience-description">
+                  {translate(lang, "home.experienceItem2Description")}
+                </p>
+              </li>
+
+              <li className="sized-list-item">
+                <h3 className="title">{translate(lang, "home.experienceItem1")}</h3>
+                <p>{translate(lang, "home.experienceItem1Year")}</p>
+                <p>{translate(lang, "home.experienceItem1Location")}</p>
+                <p className="experience-description">
+                  {translate(lang, "home.experienceItem1Description")}
+                </p>
+              </li>
+            </ul>
+          </section>
+
+        </div>
+      </div>
     </div>
   );
 }
